f42901c84ed22e25fda8ad8279972c54be3f7049
vital4u4ok <vital4u4ok@gmail.com>
2016-10-04 23:44:14 +0300

    page template

--- template.php
+++ template.php
@@ -9,9 +9,9 @@
   </head>
   
   <body>
+  	<h1>Text here</h1>
+  
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    
-    <h1>Text here</h1>
   </body>
 </html>
