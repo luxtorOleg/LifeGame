9d30759baa6b880d6127ba6258abb4b6440416a1
LuxtOleg <olegozibko@ukr.net>
2016-10-04 23:51:03 +0300

    all page are templare

--- login.php
+++ login.php
@@ -1,38 +1,40 @@
 <!DOCTYPE html>
-<html >
+<html lang="en">
 <head>
-    <link href="libs/bootstrap.min.css" rel="stylesheet" />
-    <link href="libs/bootstrap-theme.css" rel="stylesheet" />
-
-
-    <link href="css/login.css" rel="stylesheet" />
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
 <div class ="container"  >
-        <div class="col-md-12" >
-            <div class="col-md-4" >
+    <div class ="row"  >
+        <div class="col-sm-4" >
+        </div>
+        <div class="col-sm-4" >
+            <form class="form-inline">
+                <div class="form-group">
+                    <div class="labelDiv">
+                        <label for="loginInput">Login</label>
+                    </div>
+                    <input type="text" class="form-control" id="loginInput" placeholder="Login">
                 </div>
-            <div class="col-md-4" >
-                <form class="form-inline">
-                    <div class="form-group">
-                        <div class="labelDiv">
-                        <label for="inputLogin">Login</label>
-                        </div>
-                        <input type="text" class="form-control" id="inputLogin" placeholder="Login">
+                <div class="form-group">
+                    <div class="labelDiv">
+                        <label for="passwordInput">Password</label>
                     </div>
-                    <div class="form-group">
-                        <div class="labelDiv">
-                        <label for="inputPassword">Password</label>
-                        </div>
-                        <input type="password" class="form-control" id="inputPassword" placeholder="Password">
-                    </div>
-                    <button type="submit" class="btn btn-default">Sign in</button>
-                </form>
-                    </div>
-            <div class="col-md-4" >
+                    <input type="password" class="form-control" id="passwordInput" placeholder="Password">
                 </div>
-            </div>
-            </div >
+                <button type="submit" class="btn btn-default">Sign in</button>
+            </form>
+        </div>
+        <div class="col-sm-4" >
+        </div>
+    </div>
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
