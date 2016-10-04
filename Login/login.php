d830631e7ea1b77e30445f3eaa4fb9d61cd5a69c
LuxtOleg <olegozibko@ukr.net>
2016-10-04 22:17:54 +0300

    Index, Login

---
+++ Login/login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html >
+<head>
+    <link href="../Libraries/bootstrap.min.css" rel="stylesheet" />
+    <link href="../Libraries/bootstrap-theme.css" rel="stylesheet" />
+
+
+    <link href="login.css" rel="stylesheet" />
+</head>
+<body>
+<div class ="container"  >
+        <div class="col-md-12" >
+            <div class="col-md-4" >
+                </div>
+            <div class="col-md-4" >
+                <form class="form-inline">
+                    <div class="form-group">
+                        <div class="labelDiv">
+                        <label for="inputLogin">Login</label>
+                        </div>
+                        <input type="text" class="form-control" id="inputLogin" placeholder="Login">
+                    </div>
+                    <div class="form-group">
+                        <div class="labelDiv">
+                        <label for="inputPassword">Password</label>
+                        </div>
+                        <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+                    </div>
+                    <button type="submit" class="btn btn-default">Sign in</button>
+                </form>
+                    </div>
+            <div class="col-md-4" >
+                </div>
+            </div>
+            </div >
+
+</body>
+</html>
---
+++ index/index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html >
+<head>
+</head>
+<body>
+    <a href="../Login/login.php">Login</a>
+</body>
+</html>
