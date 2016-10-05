fd8943f5fc5e8ee8c0ac5d13c9c74c62ae7e3ba5
LuxtOleg <olegozibko@ukr.net>
2016-10-05 19:59:34 +0300

    modal window for login and registretion.

--- registration.php
+++ registration.php
@@ -4,16 +4,16 @@ echo  '<div id="RegistModal" class="modal fade" role="dialog" >
                 <div class="modal-content">
                     <div class="modal-body">
                         <button type="button" class="close" data-dismiss="modal">&times;</button>
-                            <div class="form-group">
-                                <input name="userName" type="text" class="form-control" placeholder="Name" >
-                                <input name="userFemale" type="text" class="form-control" placeholder="Surname">
-                                <input name="FatherName" type="text" class="form-control" placeholder="FatherName">
-                                <input name="Telephone" type="tel" class="form-control" placeholder="Phone">
-                                <input name="userEmail" type="email" class="form-control" placeholder="E-mail" >
-                                <input id = "pass" name="Password" type="password" class="form-control"placeholder="Password" >
+                            <div class="form-group registrationForm">
+                                <input name="userName" type="text" class="form-control registrationInput" placeholder="Name" >
+                                <input name="userFemale" type="text" class="form-control registrationInput" placeholder="Surname">
+                                <input name="FatherName" type="text" class="form-control registrationInput" placeholder="FatherName">
+                                <input name="Telephone" type="tel" class="form-control registrationInput" placeholder="Phone">
+                                <input name="userEmail" type="email" class="form-control registrationInput" placeholder="E-mail" >
+                                <input name="Password" type="password" class="form-control registrationInput" placeholder="Password" >
                             </div>
                             <div class="checkbox">
-                                <label>
+                                <label class="AgreeConditionsCheckbox">
                                     <input name="agreed" type="checkbox" >
                                     I agree to the terms and conditions
                                 </label>
